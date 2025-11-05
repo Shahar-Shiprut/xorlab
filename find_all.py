29f575e6d742c6435105afd244eef0d5b8f38606
Shahar-Shiprut <75133089+Shahar-Shiprut@users.noreply.github.com>
2025-11-05 17:38:25 +0200

    find all

---
+++ find_all.py
@@ -0,0 +1,21 @@
+def find_all(text: str, sub: str, overlapping: bool = True):
+    """
+    finds and returns all occurrences of `sub` in `text`.
+    :param text: the text to search in
+    :param sub: the substring to find
+    :param overlapping: whether to return overlapping results or not
+    :returns: the indices of occurrences of `sub` in `text`
+    """
+
+    indices = []
+    start = 0
+
+    while start < len(text):
+        idx = text.find(sub, start)
+        if idx < 0:
+            break
+
+        indices.append(idx)
+        start = idx + 1 if overlapping else idx + len(sub)
+
+    return indices
