0c194d2d4b813eb358dfc53ee381b39e03e7166b
Shahar-Shiprut <75133089+Shahar-Shiprut@users.noreply.github.com>
2025-11-06 11:11:12 +0200

    fix file based on feedback

--- milon_kiss.py
+++ milon_kiss.py
@@ -1,33 +1,63 @@
-def main():
-    dictionary = dict()
+PROMPT = """1. Get Word
+2. Add Word
+3. Delete Word
+4. Exit
+Enter Choice: """
+
+INPUT_GET = 1
+INPUT_ADD = 2
+INPUT_DEL = 3
+INPUT_EXT = 4
+
+dictionary = dict()
+
+
+def get_word(word: str) -> None:
+    """prints definition to `word`"""
+    if word in dictionary:
+        print(f"{word} means {dictionary[word]}")
+    else:
+        print(f"The word {word} does not exist!")
+
+
+def add_word(word: str, defi: str) -> None:
+    """adds `word` to dictionary with definition `defi`"""
+    dictionary[word] = defi
+    print(f"{word}={dictionary[word]} was added.")
+
+
+def del_word(word: str) -> None:
+    """deletes `word` from dictionary"""
+    if word in dictionary:
+        del dictionary[word]
+        print(f"{word} was erased")
+    else:
+        print(f"Word does not exist.")
+
+
+def main() -> None:
+    """run main event loop"""
+
     running = True
     while running:
-        print("1. Get Word")
-        print("2. Add Word")
-        print("3. Delete Word")
-        print("4. Exit")
-
-        inp = input("Enter Choice: ")
-        if inp == 1:
+        inp = input(PROMPT)
+        
+        if inp == INPUT_GET:
             word = input("Word to get: ")
-            if word in dictionary:
-                print(f"{word} means {dictionary[word]}")
-            else:
-                print(f"The word {word} does not exist!")
-        elif inp == 2:
+            get_word(word)
+            
+        elif inp == INPUT_ADD:
             word = input("Word to add: ")
-            dictionary[word] = input(f"Meaning of {word}: ")
-            print(f"{word}={dictionary[word]} was added.")
-        elif inp == 3:
+            add_word(word, input(f"Meaning of {word}: "))
+            
+        elif inp == INPUT_DEL:
             word = input("Word to delete: ")
-            if word in dictionary:
-                del dictionary[word]
-                print(f"{word} was erased")
-            else:
-                print(f"Word does not exist.")
-        elif inp == 4:
+            del_word(word)
+            
+        elif inp == INPUT_EXT:
             running = False
             print("Goodbye!")
+
 
 if __name__ == "__main__":
     main()
